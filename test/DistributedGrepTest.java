f297e4f9684f604009907db5a84ffbe398ffbcc1
srikanth <srikanth@srikanth-Lenovo-Z580>
2012-09-14 18:59:16 -0500

    Adding Test file

--- test/DistributedGrepTest.java
+++ test/DistributedGrepTest.java
@@ -1,5 +1,28 @@
 package edu.dsy.mp1;
 
-public class DistributedGrepTest {
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.Test;
+
+public class DistributedGrepTest {
+    @Before 
+    public void setUp () {
+    	
+    }
+    
+    @Test
+    public void testFrequentPattern() {
+    	
+    }
+    
+    @Test
+    public void testInFreqentPattern() {
+    	
+    }
+    
+    @Test
+    public void testServerFailure() {
+    }
 }
