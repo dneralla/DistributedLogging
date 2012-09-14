72444160ba4185997696cf1038c8b19c748e4e3a
srikanth <srikanth@srikanth-Lenovo-Z580>
2012-09-14 18:18:44 -0500

    Adding Log Paramters class

---
+++ src/edu/dsy/mp1/LogParameters.java
@@ -0,0 +1,21 @@
+package edu.dsy.mp1;
+
+public class LogParameters {
+	private String fileName, fileContent;
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getFileContent() {
+		return fileContent;
+	}
+
+	public void setFileContent(String fileContent) {
+		this.fileContent = fileContent;
+	}
+}
