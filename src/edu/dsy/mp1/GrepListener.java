a2067d1847b173d7ee70399fa04118f22acfb84d
dopemaster <depp.joe06@gmail.com>
2012-09-14 19:00:36 -0500

    Grep returning multiple strings

--- src/edu/dsy/mp1/GrepListener.java
+++ src/edu/dsy/mp1/GrepListener.java
@@ -1,5 +1,5 @@
 package edu.dsy.mp1;
-//Grep Listener
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -22,66 +22,68 @@ public class GrepListener {
     {
 
 	try{
-		//1. creating a server socket
-	
-	      sSocket = new ServerSocket(2060);
-		//2. Wait for connection
-		  connection = sSocket.accept();
+		
+	    sSocket = new ServerSocket(2060);
+		connection = sSocket.accept();
 		System.out.println("Connection received from " + connection.getInetAddress().getHostName());
-		//3. get Input and Output streams
 		out = new ObjectOutputStream(connection.getOutputStream());
 		out.flush();
 		in = new ObjectInputStream(connection.getInputStream());
-		
-		//4. The two parts communicate via the input and output streams
-		
-			try{
-				
-				input = (GrepInputParameters)in.readObject();
-				System.out.println(input.getFile());
-				Process p = Runtime.getRuntime().exec("grep"+" "+input.getFile()+" "+input.getPattern());
-				BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-				
-			       sendMessage(stdInput.readLine());
-
-	
+		try{   
+			  input = (GrepInputParameters)in.readObject();
+			  doGrep(input); 
 			}
-			catch(ClassNotFoundException classnot){
-				System.err.println("Data received in unknown format");
-			}
-		
-	}
-	catch(IOException ioException){
-		ioException.printStackTrace();
-	}
-	finally{
-		//4: Closing connection
-		try{
+	    catch(ClassNotFoundException e){
+		    e.printStackTrace();
+		}
+	    finally{
+	   
 			in.close();
 			out.close();
 			sSocket.close();
+		
 		}
-		catch(IOException ioException){
+	}catch(IOException ioException){
 			ioException.printStackTrace();
-		}}
-	}
+		}
+	} 
+	
  /**
   *    
   * @param msg
   */
      public void sendMessage(String msg)
      {
-	try{
+	  try{
 		out.writeObject(msg);
 		out.flush();
-		System.out.println("server>" + msg);
-	}
-	catch(IOException ioException){
+	    }
+	  catch(IOException ioException){
 		ioException.printStackTrace();
-	 }
+	    }
       }
-     public static void main(String args[])
+/**
+ * 
+ * @param input
+ * @throws IOException
+ */
+   public void doGrep(GrepInputParameters input) throws IOException
+   {
+        Process p = Runtime.getRuntime().exec("grep"+" "+input.getFile()+" "+input.getPattern());
+		BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String buffer,result=null;
+		while ((buffer = stdInput.readLine()) != null) {
+		result = result + buffer;
+		}
+		sendMessage(result);
+	        
+   }
+   
+  /**
+   *    
+   * @param args
+   */
+  public static void main(String args[])
  	{
     	System.out.println("hello");
  		GrepListener server = new GrepListener();
