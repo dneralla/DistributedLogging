0163cfc96512675166e33eb716491b8805bc854d
srikanth <srikanth@srikanth-Lenovo-Z580>
2012-09-16 02:43:12 -0500

    Modifying grep test

--- test/edu/dsy/mp1/DistributedGrepTest.java
+++ test/edu/dsy/mp1/DistributedGrepTest.java
@@ -1,51 +1,56 @@
 package edu.dsy.mp1;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 
 import org.junit.Test;
 
 public class DistributedGrepTest {
 	@Test
-	public void testGrepFunction() throws Exception {
-		String outputFileName = "output.txt";
-		String expectedOutcome = "!";
+	public void testGrepInFrequentPattern() throws Exception {
+		String outputFileName = "output1.txt", pattern = "(#!@)";
+		int expectedFrequency = 1;
 		GrepRequestDispatcher grepDispatcher = new GrepRequestDispatcher("[!@#$]*","*.log", outputFileName);
-		LogRequestDispatcher logDispatcher = new LogRequestDispatcher("InFrequentPattern.log", "!", "src/edu/dsy/mp1/config.xml");
+		LogRequestDispatcher logDispatcher = new LogRequestDispatcher("InFrequentPattern.log", "!", 1,  "src/edu/dsy/mp1/config.xml");
         logDispatcher.run();
         grepDispatcher.run();
-        assertTrue(isFileContentMatching(outputFileName, expectedOutcome));
+        assertEquals(getFrequency(outputFileName, pattern), expectedFrequency);
 	}
 
-	private boolean isFileContentMatching(String outputFileName, String result) {
-		File output =  new File(outputFileName);
-		InputStream in = null;
-        char ch = 0;
-        int count = 0;
-		try {
-			in = new FileInputStream(output);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+	@Test
+	public void testGrepFrequentPattern() throws Exception {
+		String outputFileName = "output2.txt", pattern = "(#!@)";
+		int expectedFrequency = 1000;
+		GrepRequestDispatcher grepDispatcher = new GrepRequestDispatcher(
+				"[!@#$]*", "*.log", outputFileName);
+		LogRequestDispatcher logDispatcher = new LogRequestDispatcher(
+				"FrequentPattern.log", "!", expectedFrequency,
+				"src/edu/dsy/mp1/config.xml");
+		logDispatcher.run();
+		grepDispatcher.run();
+		assertEquals(getFrequency(outputFileName, pattern), expectedFrequency);
+	}
+
+	private int getFrequency(String outputFileName, String pattern)
+			throws IOException {
+		// Open the file that is the first
+		// command line parameter
+		FileInputStream fstream = new FileInputStream(outputFileName);
+		// Get the object of DataInputStream
+		DataInputStream in = new DataInputStream(fstream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		int frequency = 0;
+		String line;
+		while ((line = br.readLine()) != null) {
+			if (line.contains(pattern))
+				frequency++;
 		}
-        Reader reader = new InputStreamReader(in);
-        try {
-			while((ch=(char) reader.read())!=-1 && count < result.length()) {
-				if(ch != result.charAt(count))
-					return false;
-				count++;
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-        if(ch == -1 && count == result.length())
-        	return true;
-        return false;
+
+		return frequency;
 	}
 }
